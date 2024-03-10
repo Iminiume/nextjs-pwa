6bf232e9e21f152f67a0b15580910339de5de4a8
Iman <aaarjmandnia@gmail.com>
2024-03-10 12:47:53 +0330

    added the manifest and pwa to the project

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,101 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="apple-mobile-web-app-capable" content="yes"></meta>
+        <meta name="theme-color" content="#2fffff"></meta>
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="default"
+        ></meta>
+
+        <meta name="mobile-web-app-capable" content="yes"></meta>
+        <meta name="apple-touch-fullscreen" content="yes"></meta>
+        <meta name="apple-mobile-web-app-title" content="Test"></meta>
+        <meta name="apple-mobile-web-app-capable" content="yes"></meta>
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="#2fffff"
+        ></meta>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/test.png" />
+        <link
+          rel="apple-touch-startup-image"
+          href="/iphonex_splash.png"
+          media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+        ></link>
+        <link
+          rel="apple-touch-startup-image"
+          href="/iphonex_splash.png"
+          media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+        ></link>
+        <link
+          rel="apple-touch-startup-image"
+          href="/iphonex_splash.png"
+          media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+        ></link>
+        <link
+          rel="apple-touch-startup-image"
+          href="/iphonex_splash.png"
+          media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+        ></link>
+        <link
+          rel="apple-touch-startup-image"
+          href="/iphonex_splash.png"
+          media="(device-width: 810px) and (device-height: 1080px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+        ></link>
+        <link
+          rel="apple-touch-startup-image"
+          href="/iphonex_splash.png"
+          media="(device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+        ></link>
+        <link
+          rel="apple-touch-startup-image"
+          href="/iphonex_splash.png"
+          media="(device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+        ></link>
+        <link
+          rel="apple-touch-startup-image"
+          href="/iphonex_splash.png"
+          media="(device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+        ></link>
+        <link
+          rel="apple-touch-startup-image"
+          href="/iphonex_splash.png"
+          media="(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+        ></link>
+        <link
+          rel="apple-touch-startup-image"
+          href="/iphonex_splash.png"
+          media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+        ></link>
+        <link
+          rel="apple-touch-startup-image"
+          href="/iphonex_splash.png"
+          media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+        ></link>
+        <link
+          rel="apple-touch-startup-image"
+          href="/iphonex_splash.png"
+          media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+        ></link>
+        <link
+          rel="apple-touch-startup-image"
+          href="/iphonex_splash.png"
+          media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+        ></link>
+        <link
+          rel="apple-touch-startup-image"
+          href="/iphonex_splash.png"
+          media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+        ></link>
+        <link
+          rel="apple-touch-startup-image"
+          href="/iphonex_splash.png"
+          media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+        ></link>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
